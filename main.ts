d8e1b4f8c6f51cea40a2c12040b24be55fd58768
murat onur karadeniz <muratonurkaradeniz@outlook.com>
2017-03-16 23:09:52 +0300

    ex

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-import {ObservableChose1,ObservableChose2  } from './react1';
+import {ObservableChose1,ObservableChose2,ObservableChose3  } from './react1';
 import { MouseEventObserve  } from './react2';
 
 ObservableChose1();
 ObservableChose2();
  MouseEventObserve;
 
+ObservableChose3();
--- react1.ts
+++ react1.ts
@@ -1,36 +1,60 @@
-import {Observable,Observer} from "rxjs";
+import { Observable, Observer } from "rxjs";
 
 export function ObservableChose1() {
-let numbers=[22,3,4,5];
+    let numbers = [22, 3, 4, 5];
 
-let source =Observable.from(numbers);
+    let source = Observable.from(numbers);
 
-var resoult=source.subscribe(
+    var resoult = source.subscribe(
 
-    p=>console.log("gelen"+ p),
-    error=>console.log(error),
-    ()=>console.log("başarılı")
-)
+        p => console.log("gelen" + p),
+        error => console.log(error),
+        () => console.log("başarılı")
+    )
 }
 
 
-export class  ObserClas  implements Observer<number> {
- 
- next(val:number){
-console.log("ObserClas"+ val )
- };
- error(error:any){}
- complete(){}
+export class ObserClas implements Observer<number> {
+
+    next(val: number) {
+        console.log("ObserClas" + val)
+    };
+    error(error: any) { }
+    complete() { }
 
 }
- 
-export   function ObservableChose2() {
-let numbers=[22,3,4,5];
 
-let source =Observable.from(numbers);
+export function ObservableChose2() {
+    let numbers = [22, 3, 4, 5];
 
-var resoult=source.subscribe( new ObserClas())
+    let source = Observable.from(numbers);
+
+    var resoult = source.subscribe(new ObserClas())
+}
+
+export function ObservableChose3() {
+
+    let numbers = [2, 3, 33, 3];
+    let source = Observable.create(x => {
+        let index = 0;
+        let val = () => {
+            x.next(numbers[index++]);
+            if (index < numbers.length) {
+                setTimeout(val, 300);
+            } else {
+                x.complete();
+            }
+        }
+        val();
+    }
+
+    ).map(t=>t*t).filter(d=>d>20);
+  source.subscribe(
+    value => console.log(`value: ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")   
+);
+
 }
 
 
- 
