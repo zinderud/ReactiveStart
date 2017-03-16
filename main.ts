e77738a429d1bfe8f82451002854a9b52c90d1cb
murat onur karadeniz <muratonurkaradeniz@outlook.com>
2017-03-17 02:03:31 +0300

    div

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 import {ObservableChose1,ObservableChose2,ObservableChose3  } from './react1';
-import { MouseEventObserve  } from './react2';
+import { MouseEventObserve,loadJson  } from './react2';
 
 ObservableChose1();
 ObservableChose2();
  MouseEventObserve;
 
 ObservableChose3();
+let cx=new loadJson().click;
--- react2.ts
+++ react2.ts
@@ -1,9 +1,38 @@
-import { Observable  } from 'rxjs';
-var output =document.getElementById("output");
-var button=document.getElementById("button");
+import { Observable } from 'rxjs';
+
 export var MouseEventObserve = Observable.fromEvent(document, "mousemove");
 
 MouseEventObserve.subscribe(
-    value=>console.log("mouse location: x ",value.layerX,"mouse location: y ",value.layerY)
+    e => console.log("mouse location: x ", e.layerX, "mouse location: y ", e.layerY)
 )
-  
+
+export class loadJson {
+    output = document.getElementById("output");
+    button = document.getElementById("button");
+
+
+    load(url: string) {
+
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            data.forEach(m => {
+                let div = document.createElement("div");
+                div.innerText = m._id;
+                this.output.appendChild(div)
+                
+                let div2 = document.createElement("div");
+                div2.innerText = m.name;
+                this.output.appendChild(div2)
+            });
+        });
+        xhr.open("Get", url);
+        xhr.send();
+    }
+
+    click = Observable.fromEvent(this.button, "click").subscribe(
+        e => this.load("ss.json"),
+        error => console.log(error),
+        () => console.log("ok")
+    )
+}
